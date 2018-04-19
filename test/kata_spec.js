6130daf45896e01dedd47573cbce04c0fda2abd3
patrickgilbride <gilbride22@gmail.com>
2018-04-18 20:52:28 -0400

    troubleshooting

--- test/kata_spec.js
+++ test/kata_spec.js
@@ -1,10 +1,10 @@
 describe("Person", function () {
-  it("has a greeting", function() {
-    var person = new Person();
-    expect(person.greetGeneral).toBe("Something that is not Hello!");
-  });
+    it("has a greeting", function () {
+        var person = new Person();
+        expect(person.greetGeneral).toBe("Hello!");
+    });
     it("has a greeting", function () {
         var person = new Person("Patrick");
-        expect(person.greetSpecific).toBe("Something that is not Hello Patrick!");
+        expect(person.greetSpecific).toBe("Hello Patrick!");
     });
 });
